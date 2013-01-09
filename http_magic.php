54eddf0cac6e9b23989b238eab70db9d4ee7a74f
Jehy <fate@jehy.ru>
2013-01-09 06:18:28 +0400

    first commit

---
+++ http_magic.php
@@ -0,0 +1,414 @@
+<?
+#
+# HTTP_MAGIC.PHP v.25/05/11   v3.82
+# функции для работы с протоколом HTTP/SSL и данными в HTML.
+#
+#
+#    http($request,$port=80,$ssl=false,$read=true) # общая функция для запросов.
+#
+#         $request      -       полный http запрос
+#         $port         -       можно указать порт коннекта
+#         $ssl          -       true/false (ssl или обычный)
+#         $read         -       true/false/header (читать, не читать, читать только заголовок ответа)
+#
+#    http_get($addr,$cookies=array(),$referer='',$useragent='....',$ssl=false,$read=true)     ! в версии 3.5 функцию не проверял
+#
+#         $addr         -       адрес скачиваемой страницы (http://www.domain.com/page.html или www.domain.com/page.html)
+#         $cookies      -       можно передать стандартный массив кук, полученный функцией cookies($header);
+#         $referer      -       можно передать реферала страницы
+#         $useragent    -       можно передать юзерагент
+#         $port         -       можно указать порт коннекта
+#         $ssl          -       true/false (ssl или обычный)
+#         $read         -       true/false/header (читать, не читать, читать только заголовок ответа)
+#
+#
+#    cookies($header,$cookies=array()) # Экстракт и синхронизация кукис
+#
+#         $header       -       свежеполученный хидер
+#         $cookies      -       массив с куками извлеченными ранее при помощи cookies()
+#         для использования кукисов в запросах используйте $cookie=implode("\r\n",$cookies);
+#
+#
+#    form_extractor($html,$clear=true)
+#
+#       Добывает все инпуты из всех форм (работает только с тегами <input, текстарии и прочие селекты не канают, значения берёт только указанные)
+#       После чего выдёргивает из них все пропертисы и в случае если указана $clear=true (по умолчанию указана!) оставляет только name и value
+#       Соответственно в результате либо массив массивов знаений инпутов[form_i]=>[input_name]=>[input_value]
+#       либо массив массивов массивов [form_i]=>[input_name]=>[input_prop_name]=>[input_prop_value];
+#
+#       $html - код страницы
+#       $clear - true (только имена и значения инпутов) / false (все свойства инпутов массивом)
+#
+#
+#    tag_props_extractor($html)
+#       Добывает из тега все пропертисы!.
+#       Тег ($html) должен быть голый.
+#       т.е. из
+#               <a href="..." target="..." color="...">...</a>
+#       нужно только
+#               href="..." target="..." color="..."
+#
+#
+#
+#
+#    TODO:
+#       * Добавить возможность сохранять, загружать, загружать последнюю сохранённую сессию - т.е. куки
+#       * Добавить возможность делить куки по доменам внутри единого контейнера (отдельную функцию типа cookies_domain($cookies,)
+#
+
+
+
+function http($request,$port=80,$ssl=false,$read=true) # общая функция.
+{
+    #    echo eee('HTTP Request: ',$request);
+        $addr=explode('host: ',strtolower($request),2);
+        $addr=explode("\n",$addr[1],2);
+        $addr=trim($addr[0]);
+
+        $f=http_open($addr,$port,$ssl); # новый
+        if($f==false)return $f;
+
+        @stream_set_blocking($f,1);
+
+        fwrite($f, $request); # отправляем запрос
+
+        if($read==false)return;
+        $data='';
+        while(true)
+        {
+                $line=fgets($f);
+                $data.=$line;
+                if($line==="\r\n")
+                {
+                        $method=http_method($data);
+                        break;
+                }
+        }
+
+        if($read==='header')return $data;
+
+        if($method==='chunked') # читаем методом CHUNKED
+        {
+                $data.=http_read_chunked($f);
+        }
+        else
+        {
+                $data.= http_read_all($f); # читаем обычным методом
+        }
+        fclose($f);
+     #   echo eee('HTTP Result: ',$data);
+
+        return $data;
+}
+
+function http_get($addr,$cookies=array(),$referer='',$useragent='Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.15) Gecko/20110303 MRA 5.7 (build 03686) Firefox/3.6.15')
+{
+        # Для ускорения работы с методом GET
+        $x=explode('//',$addr,2);
+        if(strtolower($x[0])=='http:')$addr=$addr[1];
+
+        $addr=explode('/',$addr,2);
+
+        $query = 'GET /' . $addr[1] . ' HTTP/1.1' . "\r\n" .
+                 'Host: ' . $addr[0] . "\r\n" .
+                 'User-Agent: ' . $useragent . "\r\n";
+
+        if($cookie!='')
+        $query.= 'Cookie: ' . implode('; ',$cookies) . "\r\n";
+
+        if($referer!='')
+        $referer.= 'Referer: ' . $referer . "\r\n";
+
+        $query.= 'Connection: close' . "\r\n" .
+                 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' . "\r\n" .
+                 'Accept-Language: en-us,en;q=0.7,ru;q=0.3' . "\r\n" .
+                 'Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.7' . "\r\n" . "\r\n";
+
+        $res=http($query); # http_v_3
+        return $res;
+}
+
+function http_method($header)
+{
+        $header=header_parser($header);
+        $method=strtolower($header['transfer-encoding']);
+        if($method!='chunked')return 'plain'; else return 'chunked';
+}
+
+function http_read_all($f) # читает документ целиком в обычном режиме. для ф-ии HTTP
+{
+        $data='';
+        while (!feof($f))$data.=fgets($f, 16384);
+        return $data;
+}
+
+function http_error($e='')
+{
+        if(!$e)$e=@error_get_last();else $e['message']=$e;
+        if(!$e)$e=$php_errormsg;
+        echo '<div style="padding:20px;background-color:#FFFFCC;border:1px solid silver">http_magic error: <b>' . $e['message'] . '</b></div>';
+}
+
+function http_open($addr,$port=80,$ssl=false) # функция открытия соединения. для ф-ии HTTP
+{
+        if($ssl)$addr='ssl://' . $addr;
+        $f=@fsockopen($addr, $port, $errno, $errstr, 5);
+        if($f===false)http_error();
+        return $f;
+}
+
+function http_read_chunked($f)  # адекватное чтение в случае режима CHUNKED (нужно обязательно при чтении картинок)
+{
+        $length=fgets($f);
+        $length=hexdec($length);
+
+        while(true)
+        {
+                if ($length < 1)break;
+                $x='';
+                while(strlen($x)<$length)
+                {
+                        $b=fread($f, $length-strlen($x));
+                        if($b=='')return $data;
+                        $x.=$b;
+                }
+                $data.= $x;
+                fgets($f);
+                $length = rtrim(fgets($f));
+                $length = hexdec($length);
+        }
+        return $data;
+}
+
+
+function header_parser($header) # Парсит хидеры и достаёт из них всё ценное.
+{
+        $headers=explode("\r\n\r\n",$header,2);
+        $headers=explode("\r\n",trim($headers[0]));
+        $http=trim(array_shift($headers));
+        $harray=array(); #all headers
+        $cookies=array(); #cookies
+        foreach($headers as $i=>$header)
+        {
+                $header=explode(':',trim($header),2);
+                $type=strtolower($header[0]);
+                $value=trim($header[1]);
+                if($type=='set-cookie')
+                {
+                        $value=explode(';',$value,2);
+                        $harray['set-cookie'][]=$value[0];
+                }
+                else
+                {
+                        $harray[$type]=$value;
+                }
+        }
+        return $harray;
+}
+
+function extract_charset($header) # из хидера вытаскивает название кодировки
+{
+        $charset=explode('charset=',$header,2);
+        $charset=explode("\r\n",$charset[1],2);
+        $charset=explode(';',$charset[0],2);
+        $charset=$charset[0];
+        return $charset;
+}
+
+function cookies($header,$cookies=array()) # Возвращает массив кук. элементы вида cookeis[0]="name=value"; Может совмещать куки с массивом cookies, такого же вида.
+{
+        $headers=explode("\r\n\r\n",$header,2);
+        $headers=explode("\r\n",trim($headers[0]));
+        $http=trim(array_pop($headers));
+        foreach($headers as $i=>$header)
+        {
+                $header=explode(':',trim($header),2);
+                $type=strtolower($header[0]);
+                $value=trim($header[1]);
+                if($type=='set-cookie')
+                {
+                        $value=explode(';',$value,2);
+                        $cookies[]=$value[0];
+                }
+        }
+        return cl_cookies($cookies);
+}
+
+function cl_cookies($cookies) # "чистит" массив кук. Вспомогательная для cookies
+{
+        $cl=array();
+        foreach($cookies as $c)
+        {
+                $c=explode('=',$c,2);
+                $cl[$c[0]]=$c[1];
+        }
+        $cookies=array();
+        foreach($cl as $name=>$c)
+        {
+                $cookies[]=$name . '=' . $c;
+        }
+        return $cookies;
+
+}
+
+# Добывает все инпуты из всех форм (работает только с тегами <input, текстарии и прочие селекты не канают, значения берёт только указанные)
+# После чего выдёргивает из них все пропертисы и в случае если указана $clear=true (по умолчанию указана!) оставляет только name и value
+# Соответственно в результате либо массив массивов знаений инпутов[form_i]=>[input_name]=>[input_value]
+# либо массив массивов массивов [form_i]=>[input_name]=>[input_prop_name]=>[input_prop_value];
+function form_extractor($html,$clear=true)
+{
+        $forms=array();
+        $offset=0;
+        $id=0;
+        while(true)
+        {
+                $find=stripos($html,'<form ',$offset);
+                if($find===false)break;
+                $start=$find;
+                $end=stripos($html,'</form',$start);
+                if($end==false)$end=strlen($html)-1;
+                $offset=$end;
+                $form=substr($html,$start+5,$end-$start-5);
+                $inputs=input_extractor($form);
+                $form=substr($form,0,strpos($form,'>'));
+                $form=tag_props_extractor($form);
+                $name=$form['name'];
+                if($name=='')$name=$form['id'];
+                if($name==''||isset($forms[$name]))$name=$id++;
+                $forms[$name]=array('properties'=>$form,'inputs'=>$inputs);
+        }
+        return $forms;
+}
+
+# Добывает все инпуты из формы (работает только с тегами <input, текстарии и прочие селекты не канают, значения берёт только указанные)
+# После чего выдёргивает из них все пропертисы и в случае если указана $clear=true (по умолчанию указана!) оставляет только name и value
+# Соответственно в результате либо массив name=>value либо массив массивов name=>array(...props...);
+# Exclude_image - исключает попадание input type=image в результаты. Вхуй! Заебали тут толпиться!
+function input_extractor($html,$clear=true,$exclude_image=true)
+{
+        $inputs=array();
+        $offset=0;
+        while(true)
+        {
+                $find=stripos($html,'<input ',$offset);
+                if($find===false)break;
+
+                $start=$find;
+                $end=stripos($html,'>',$start);
+                if($end==false)$end=strlen($html)-1;
+                $offset=$end;
+
+                $input=substr($html,$start+6,$end-$start-6);
+                $props=tag_props_extractor($input);
+                if(strtolower($props['type'])!='image'||$exclude_image==false)
+                {
+                        if($clear)
+                        {
+                                # name=>value!
+                                if($props['name']!='')$inputs[$props['name']]=$props['value'];
+                        }
+                        else
+                        {
+                                # name=>array();
+                                if($props['name']!='')$inputs[$props['name']]=$props;
+                        }
+                }
+
+        }
+        return $inputs;
+}
+
+# Добывает из тега все пропертисы!.
+# Тег должен быть голый.
+# т.е. из
+#        <a href="..." target="..." color="...">...</a>
+# нужно только
+#        href="..." target="..." color="..."
+function tag_props_extractor($html)
+{
+        $html=trim($html);
+        $props=array();
+
+
+        $begin=false;
+        $char='';
+        $name='';
+        $value='';
+        $i=0;
+        for($i=$i;$i<strlen($html);$i++)
+        {
+                # Если найден пробел, значит мы нашли аттрибут без значения
+                if($html[$i]==' ')
+                {
+                        if(trim($name)!='')$props[$name]='';
+                        $name='';
+                        # После аттрибута может быть куча пробелов
+                        while($html[$i]===' ')$i++;
+                }
+                if($html[$i]=='=')
+                {
+                        # Begin!;
+                        $i++;
+                        $char=$html[$i];
+                        if($char=='"'||$char=="'")
+                        {
+                                # Нормальные разделители
+                                $i++; # Сдвиг
+                        }
+                        elseif($char==' ')
+                        {
+                                # Ну не должно там быть пробела никогда
+                                echo '<font color=red>Ебанутый инпут, хуй знает что будет за результат: <b>' . $html . '</b></font><Br>';
+                                if(trim($name)!='')$props[$name]='';
+                                $name='';
+                                $char=false;
+                        }
+                        else
+                        {
+                                # Разделителя нету... значит разделитель в конце пробел
+                                $char=' ';
+                        }
+                        # Разделитель определён, теперь поймать значение учитывая конец
+                        while(true&&$char!==false)
+                        {
+                                if($html[$i]=='')break;
+                                if($html[$i]==$char)break;
+                                # Если тот кто вызвал функцию мудак, то концом обеда будет >
+                                if($char==' ' && $html[$i]=='>')break;
+                                $value.=$html[$i];
+                                $i++;
+                        }
+                        $name=strtolower($name);
+                        if($name=='name')$value=Cleaner($value);
+                        $props[strtolower(trim($name))] = trim($value);
+                        //if($name=='name')echo '&' . $value .  '(' . strlen($value).')=&<br>';
+                        $name='';
+                        $value='';
+                }
+                else
+                {
+                        # Часть имени
+                        $name .= $html[$i];
+                }
+        }
+        return $props;
+}
+
+function Cleaner($t)
+{
+        $alp='abcdefghijklmnopqrstuvwxyz1234567890_-[](){}%&.,=+';
+        $a2='';
+        for($i=0;$i<strlen($t);$i++)
+        {
+                if(stripos($alp,$t[$i])!==false)$a2.=$t[$i];
+                else echo $t[$i] . '<br>';
+        }
+        return $a2;
+}
+
+
+
+
+
+
+?>
---
+++ ingress_magic.php
@@ -0,0 +1,133 @@
+<?
+function set_log($name)
+{
+  global $LOG_FILE;
+  $LOG_FILE=$name.'_'.time().'.txt';
+}
+function add_log($text,$output=0)
+{
+  global $LOG_FILE;
+  $text.="\n";
+  if($output)
+    echo '<br>'.$text;
+  if($LOG_FILE)
+  {
+    $fname='cache/'.$LOG_FILE;
+    file_put_contents($fname,$text, FILE_APPEND);
+  }
+}
+function send_passcode($cookie,$token,$code,$force=false)
+{
+  $query2='{"passcode":"'.$code.'","method":"dashboard.redeemReward"}';
+  $len=strlen($query2);
+  
+$query='POST /rpc/dashboard.redeemReward HTTP/1.1
+Host: www.ingress.com
+Connection: close
+Content-Length: '.$len.'
+Origin: http://www.ingress.com
+X-Requested-With: XMLHttpRequest
+X-CSRFToken: '.$token.'
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11
+Content-Type: application/json; charset=UTF-8
+Accept: application/json, text/javascript, */*; q=0.01
+Referer: http://www.ingress.com/intel
+Accept-Language: en-US,en;q=0.8,ru;q=0.6
+Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.3
+Cookie: '.$cookie.'
+
+'.$query2;
+$fname='cache/code_'.($code).'.txt';
+if($force)
+  $fname='cache/code_force_'.($code).'.txt';
+if(!file_exists($fname))
+{
+  $json=( http($query));
+  @mkdir('cache',0777);
+  #in case of fail
+  if(strpos($json,'error code 503')!==FALSE)
+  {
+    add_log('Failed to make send password request (503), repeating...',1);
+    sleep(3);
+    $json=send_passcode($cookie,$token,$code,$force);  
+  }
+  file_put_contents($fname,$json);
+  return $json;
+}
+else
+  #$json=file_get_contents($fname);
+  return 'Already sent';
+}
+
+
+function get_log($cookie,$token)
+{
+
+        $useragent='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11';
+        
+        $referer='http://www.ingress.com/intel';
+        
+        $path='/rpc/dashboard.getPaginatedPlextsV2';
+        
+        $host='www.ingress.com';
+        
+        $query = 'POST ' . $path . ' HTTP/1.1' . "\r\n" . 
+                 'Host: ' . $host . "\r\n" . 
+                 'User-Agent: ' . $useragent . "\r\n";  
+        
+        if($cookie!='') $query.=
+                 'Cookie: ' . $cookie . "\r\n"; 
+        
+        if($referer!='') $query.=
+                 'Referer: ' . $referer . "\r\n"; 
+        
+        $query.= 'Connection: close' . "\r\n" . 
+                 'Origin: http://www.ingress.com' . "\r\n" . 
+                 'X-Requested-With: XMLHttpRequest' . "\r\n" . 
+                 'X-Csrftoken: '.$token . "\r\n" . 
+                 'Content-Type: application/json; charset=UTF-8' . "\r\n" . 
+                 'Accept: application/json, text/javascript, */*; q=0.01' . "\r\n" . 
+                 'Accept-Language: en-US,en;q=0.8,ru;q=0.6' . "\r\n" . 
+                 'Accept-Charset: windows-1251,utf-8;q=0.7,*;q=0.3' . "\r\n" ;
+        
+#search global area
+$query2= '{"desiredNumItems":300,"minLatE6":-90000000,"minLngE6":-180000000,"maxLatE6":90000000,"maxLngE6":180000000,"minTimestampMs":-1,"maxTimestampMs":-1,"method":"dashboard.getPaginatedPlextsV2"}';
+
+
+$query.= 'Content-Length: ' . strlen($query2)  . "\r\n\r\n";
+                $query.= $query2;
+                
+                
+$fname='cache/chat_'.time().'.txt';
+if(!file_exists($fname))
+{
+  $json=( http($query));
+  @mkdir('cache',0777);
+  
+  if(strpos($json,'error code 503')!==FALSE)
+  {
+    add_log('Failed to make send chat log request (503), repeating...',1);
+    sleep(3);
+    $json=get_log($cookie,$token);  
+  }
+  file_put_contents($fname,$json);
+  return $json;
+}
+else
+  return false;
+  #$json=file_get_contents($fname);
+}
+function f_connect()
+{
+  $db = mysql_connect('localhost', 'root', '');
+  mysql_select_db('ingress', $db);
+}
+function check_badgerov($code)
+{
+  $result=mysql_query('select 1 from ingress_words where word="'.addslashes($code).'"');
+  if(mysql_num_rows($result))
+    return 1;
+  return 0;
+}
+
+?>
